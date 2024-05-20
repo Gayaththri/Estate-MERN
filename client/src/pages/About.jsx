2e5c806e81a98628d1cfcfb9ca6b3d671655892f
Gayaththri <sriigaya3@gmail.com>
2024-05-20 17:38:00 +0530

    modified and altered the names

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,15 +4,14 @@ export default function About() {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800">
-        About Gayaththri Estate
+        About Homescape
       </h1>
       <p className="mb-4 text-slate-700">
-        Welcome to Gayaththri Estate, where tranquility meets luxury. Gayaththri
-        Estate is a leading real estate agency that specializes in helping
-        clients buy, sell, and rent properties in the most desirable
-        neighborhoods. Our team of experienced agents is dedicated to providing
-        exceptional service and making the buying and selling process as smooth
-        as possible.
+        Welcome to Homescape, where tranquility meets luxury. Homescape is a
+        leading real estate agency that specializes in helping clients buy,
+        sell, and rent properties in the most desirable neighborhoods. Our team
+        of experienced agents is dedicated to providing exceptional service and
+        making the buying and selling process as smooth as possible.
       </p>
       <p className="mb-4 text-slate-700">
         Our mission is to help our clients achieve their real estate goals by
